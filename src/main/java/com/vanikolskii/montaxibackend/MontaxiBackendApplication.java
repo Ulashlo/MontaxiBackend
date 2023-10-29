c9fc3f7917d1d6451242e5e02210235e4dc69c39
vnikolskiy <vnikolskiy@kameleoon.com>
2023-10-29 22:19:10 +0300

    #1 Init

---
+++ src/main/java/com/vanikolskii/montaxibackend/MontaxiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vanikolskii.montaxibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontaxiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MontaxiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vanikolskii/montaxibackend/MontaxiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanikolskii.montaxibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontaxiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
